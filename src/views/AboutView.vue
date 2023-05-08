cccb58aea7f8fed5c1b4288ee55608cb4869b5a1
Braian Vargas <braianvargas1616@gmail.com>
2023-05-08 17:01:14 -0300

    [ADD] Clientes y reconocimientos

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -28,6 +28,31 @@
           img_src: "src/assets/images/team/LUIS.png"
         }
       ]
+
+      const premiosArray = [
+        {
+          title:"Premio Mercurio de Oro 2018-2019",
+          slogan:"Hace 50 años atrás se creó este prestigioso premio, que año a año distingue a las mejores y más prestigiosas empresas y comercios de San Juan.",
+          subtitle:"Reconocimiento a la mejor asesoría y comercialización de equipos tecnológicos.",
+          picture1:"src/assets/images/eventos/mercurio_1.png",
+          picture2:"src/assets/images/eventos/mercurio_2.png"
+        },
+        {
+          title:"Premio Mercurio de Oro 2020-2023",
+          slogan:"Hace 50 años atrás se creó este prestigioso premio, que año a año distingue a las mejores y más prestigiosas empresas y comercios de San Juan.",
+          subtitle:"Reconocimiento a la mejor asesoría y comercialización de equipos tecnológicos.",
+          picture1:"src/assets/images/eventos/mercurio_1.png",
+          picture2:"src/assets/images/eventos/mercurio_2.png"
+        },
+        {
+          title:"Premio Mercedario",
+          slogan:"El premio de mayor trayectoria a nivel nacional, entregado en forma ininterrumpida desde 1968 a las empresas más populares para los sanjuaninos.",
+          subtitle:"Reconocimiento a la mejor asistencia técnica profesional de San Juan.",
+          picture1:"src/assets/images/eventos/mercedario_1.png",
+          picture2:"src/assets/images/eventos/mercedario_2.png"
+        },
+      ]
+
       const imageNames = Array.from({length: 20}, (value, index) => `${index+1}`)
 
       const groupsOfThree = [];
@@ -37,7 +62,8 @@
       return {
         teamArray,
         groupsOfThree,
-        imageNames
+        imageNames,
+        premiosArray
       }
     },
     mounted() {
@@ -62,7 +88,7 @@
 </script>
 
 <template>
-  <section id="about_us" class="">
+  <section id="about_us">
     <h2 class="m-auto text-center p-3 bolder fs-1">Sobre Nosotros</h2>
     <div class="container m-auto p-3">
       <div class="row">
@@ -86,6 +112,44 @@
       </div>
     </div>
     <!-- carrousel of Team -->
+    <section id="carrousel_people" class="p-4">
+      <div id="carousel-team" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" v-for="person in teamArray">
+            <img class="card-img-top d-flex m-auto" v-bind:src="person.img_src" alt="card image cap" style="width: 200px; height: 200px;">
+            <div class="card">
+              <div class="card-body">
+                <p class="card-title text-black">
+                  {{person.name}}
+                </p>
+                <p class="card-text">
+                  {{person.rol}}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-team"
+          data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel-team"
+          data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </section>
+
+
+
+
+
+<!-- ------------------------------------------------------------------------------------------------------ -->
+
+
+
     <section id="carrousel_people" class="p-4">
       <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner" v-if="getScreenRes() == true">
@@ -132,7 +196,7 @@
   </section>
   
   <section id="portfolio">
-    <div class="container pb-5 pt-5">
+    <div class="container pb-5 pt-5 row m-auto">
       <div class="text-container col-12 col-md-4">
         <h2 class="text-white d-flex m-auto bolder" >
           Nuestros Clientes
@@ -144,14 +208,37 @@
           Gracias a todos por confiar en nosotros.
         </p>
       </div>
-      <div class="img-container col-12 col-md-8 bg-white">
-        <div class="row p-3 m-2" v-for="image in imageNames">
-          <div class="col-12 col-md-4 m-auto" >
-            <img :src="`src/assets/images/empresas/${image}.png`" :alt="imageName">
+      <div class="img-container col-12 col-md-7 bg-white">
+        <div class="p-3 row m-auto">
+          <div class="col-12 d-flex col-md-4 m-auto" v-for="image in imageNames">
+            <img class="container p-2" :src="`src/assets/images/empresas/${image}.png`" :alt="imageName">
           </div>
         </div>
       </div>
-      
+    </div>
+  </section>
+  <section id="reconocimientos">
+    <div class="bg-black d-block m-auto">
+      <h1 class="text-white m-auto text-center p-4">Reconocimientos</h1>
+      <div class="row d-flex m-auto mt-4 p-2" style="border:2px solid #FFFF; border-radius: 15px"  v-for="premio in premiosArray">
+        <div class="col-12 col-md-4 p-2" >
+          <div class="card">
+            <img v-bind:src="premio.picture1" alt="" class="card-img-top">
+            <div class="card-body">
+              <p class="card-title text-black">
+                {{premio.title}}
+              </p>
+              <p class="card-text">
+                {{premio.subtitle}}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-8 p-2 container">
+          <img v-bind:src="premio.picture2" class="w-100" alt="" style="max-width:800px">
+        </div>
+        <hr/>
+      </div>
     </div>
   </section>
 
@@ -176,20 +263,21 @@
     color: #FE5000;
     font-size: 80px;
   }
-
-  #about_us {
-    color: white;
-
+  #about_us{
     background-image: linear-gradient(rgba(8, 7, 7, 0.589), rgba(0, 0, 0, 0.801)), url(../assets/images/towers.webp);
+    color: white;
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
   }
+  
 
   #portfolio{
     background-color: gray;
   }
-
+  .img-container{
+    border-radius: 17px;
+  }
   .img-container img{
     max-width: 120px;
   }
